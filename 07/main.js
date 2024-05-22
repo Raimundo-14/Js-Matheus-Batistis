fc1ad7d0cabbe4fd0e412ffc43f5b379df47289c
Raimundo <raimundoalbuquerque14@hotmail.com>
2024-05-21 21:26:29 -0300

    Exercício 07

---
+++ 07/main.js
@@ -0,0 +1,15 @@
+let i = 0;
+
+while (i <= 10) {
+  console.log(i);
+
+  i++;
+}
+
+for (let index = 100; index >= 50; index--) {
+  console.log(index);
+}
+
+for (ii = 0; ii <= 20; ii++) {
+  console.log(ii);
+}
